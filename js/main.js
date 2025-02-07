da08476d29a16fc3c24c172b3859cfa48faff5ae
KoNstantin_Binevskii <kbynevskij@yandex.ru>
2025-02-07 23:42:59 +0100

    добавление страницы в scss

---
+++ js/main.js
@@ -0,0 +1,80 @@
+const data = JSON.parse(dataInfo);
+const cartBox = document.querySelector('.cart-box');
+
+// Функция добавления товара в корзину
+function addToCart(item) {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push(item);
+    localStorage.setItem('cart', JSON.stringify(cart));
+    console.log('Товар добавлен в корзину:', item);
+}
+
+data.forEach(element => {
+    const bodyEl = document.createElement('div');
+    bodyEl.classList.add('bodyContent'); 
+
+    const image_Conteiner = document.createElement('div');
+    image_Conteiner.classList.add('image_container');
+
+    const overley = document.createElement('div');
+    overley.classList.add('overley');
+
+    const svgButton = document.createElement('img');
+    svgButton.classList.add('svgButton');
+    svgButton.alt = 'svg';
+    svgButton.src = './svg/kassa.svg'
+    svgButton.src = '../svg/kassa.svg'
+
+    const itemImg = document.createElement('img');
+    itemImg.classList.add('img___sale');
+    itemImg.src = element.url;
+    itemImg.alt = 'Add to Cart';
+
+    const itemContent = document.createElement('div');
+    itemContent.classList.add('item__content');
+
+    const itemHead = document.createElement('h4');
+    itemHead.classList.add('item__content_text');
+    itemHead.textContent = element.name;
+
+    const itemText = document.createElement('p');
+    itemText.classList.add('item__content_text2');
+    itemText.textContent = element.textInfo;
+
+    const itemPrice = document.createElement('p');
+    itemPrice.classList.add('item__content_price');
+    itemPrice.textContent = `$${element.price}`;
+
+    const button = document.createElement('button');
+    button.classList.add('button');
+    const icon = document.createElement('i');
+    icon.classList.add('fas', 'fa-shopping-cart');
+    button.appendChild(icon);
+    const buttonText = document.createTextNode(' Add to Cart');
+    button.appendChild(buttonText);
+
+    button.addEventListener('click', () => {
+        addToCart({
+            name: element.name,
+            price: element.price,
+            url: element.url,
+            color: element.color,
+            size: element.size
+        });
+        displayCartItems();
+    });
+    
+    cartBox.appendChild(bodyEl);
+ 
+    
+    bodyEl.appendChild(image_Conteiner);
+    image_Conteiner.appendChild(itemImg);
+    bodyEl.appendChild(overley);
+
+    bodyEl.appendChild(itemContent);
+    overley.appendChild(button);
+    button.appendChild(svgButton);
+    itemContent.appendChild(itemHead);
+    itemContent.appendChild(itemText);
+    itemContent.appendChild(itemPrice);
+});
---
+++ slide_side/js/data_for_side.js
@@ -0,0 +1,27 @@
+const dataInfo = `
+    [
+        {
+            "name": "ELLERY X M'O CAPSULE",
+            "url": "../png/4.png",
+            "textInfo": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "300.00",
+            "color": "black",
+            "size": "XL"
+        },
+         {
+            "name": "ELLERY X M'O CAPSULE",
+            "url": "../png/3.png",
+            "textInfo": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "52.00",
+            "color": "black",
+            "size": "M"
+        },
+        {
+            "name": "ELLERY X M'O CAPSULE",
+            "url": "../png/6.png",
+            "textInfo": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "55.00",
+             "color": "black",
+            "size": "XXL"
+}]
+`
